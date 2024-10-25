7a50d9759a3927adb198e8d50528fd61a37b9002
Ryan Harrington <rharrington@microsoft.com>
2024-10-25 13:25:19 -0400

    Updated packaging structure

--- setup.py
+++ setup.py
@@ -1,12 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name='table_writer',
-    version='1.0.0',
-    packages=find_packages(),
-    install_requires=[
-        'pyodbc',
-        'azure-keyvault-secrets',
-        'azure-identity'
-    ],
-)
+if __name__ == "__main__":
+    setup()
---
+++ tests/test.py
@@ -0,0 +1,4 @@
+from sql_utils.table_writer import ADFpipelinesSQL_Logger
+
+
+print('Hello')
